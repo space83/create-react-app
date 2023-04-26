257f1b958c589eeae817048a0aeb6ebbdd8d076c
mazlan.ibrahim <mazlan.ibrahim@uberfusion.com>
2023-04-26 10:20:44 +0800

    8.7 Creating a Layout Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div>
+import React, { Component } from 'react';
 
-    </div>
-  );
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
