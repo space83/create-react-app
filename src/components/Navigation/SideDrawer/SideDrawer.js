5f25fe4b33f3f08f6750bb1025ae3b3f6e1cb729
mazlan.ibrahim <mazlan.ibrahim@uberfusion.com>
2023-04-27 13:58:32 +0800

    8:36

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,27 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux_';
 
 const sideDrawer = (props) => {
-    return(
-        <div className={classes.SideDrawer}>
-            <Logo />
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={classes.SideDrawer}>
+                <div className={attachedClasses.join(' ')}>
+                    <Logo />
+                </div>
+                
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
     );
 }
 export default sideDrawer;
