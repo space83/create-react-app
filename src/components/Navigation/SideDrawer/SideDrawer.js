dc06cddd737d82e0ccffc9b914ef72752a2b9c91
mazlan.ibrahim <mazlan.ibrahim@uberfusion.com>
2023-04-27 14:36:05 +0800

    End chapter 8

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux_';
+import Aux from '../../../hoc/Aux_/Aux_';
 
 const sideDrawer = (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
